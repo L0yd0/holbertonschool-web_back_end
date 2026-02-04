1d6c6b766030fdd5c37a9915af3a4c3ba74620a1
Ricardo Vargas <ricardovargas120717@gmail.com>
2026-02-03 20:23:17 -0600

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,20 +2,19 @@
 """ Module for using PyMongo to parse nginx logs """
 from pymongo import MongoClient
 
-# default host:port is localhost:27017
-client = MongoClient()
-col = client.logs.nginx
-
-# have to use empty {} to get count of all docs!
-count = col.count_documents({})
-get = col.count_documents({"method": "GET"})
-post = col.count_documents({"method": "POST"})
-put = col.count_documents({"method": "PUT"})
-patch = col.count_documents({"method": "PATCH"})
-delete = col.count_documents({"method": "DELETE"})
-status = col.count_documents({"method": "GET", "path": "/status"})
 
 if __name__ == "__main__":
+    client = MongoClient()
+    col = client.logs.nginx
+    
+    count = col.count_documents({})
+    get = col.count_documents({"method": "GET"})
+    post = col.count_documents({"method": "POST"})
+    put = col.count_documents({"method": "PUT"})
+    patch = col.count_documents({"method": "PATCH"})
+    delete = col.count_documents({"method": "DELETE"})
+    status = col.count_documents({"method": "GET", "path": "/status"})
+    
     print(f"{count} logs")
     print("Methods:")
     print(f"    method GET: {get}")
