6d20e1e982eac4b1d3f5f4dbc7bfe47ce41bcb2f
Ricardo Vargas <ricardovargas120717@gmail.com>
2026-02-10 16:44:53 -0600

    Modify users table to use AUTO_INCREMENT for id

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
--- creates users table 
-
+-- Creates a table users with id, email and name
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     PRIMARY KEY (id)
-)
+);
